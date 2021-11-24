dabb3b5325ca48dacf26e80a4906e0c55fb77baf
rebleo <rml444@nyu.edu>
2021-11-24 11:31:55 -0500

    library

---
+++ week12/code/theMachine.js
@@ -0,0 +1,25 @@
+console.log("js!!");
+
+let theP = "yr a hero!";
+
+$('h3').on("click", function(){
+    console.log("hello?")
+})
+
+
+
+function potato() {
+    console.log("potato click!");
+    $('h3').append(theP)
+    $('h4').app$("#myButton").on('click', potato);end("<p>"+ theP + "</p>");
+  
+}
+
+$('#clickHere').click(function() {
+    console.log("yellow click");
+    $('#other').text("now the green text is this");
+    $('div').css("background-color", "red")
+    $('body').css("background-color", "blue");
+    $('h3').remove(":contains('hello')");
+
+})
